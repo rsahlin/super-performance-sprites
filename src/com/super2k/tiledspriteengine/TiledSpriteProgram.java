381c4298f9c0f0ceb40117480b3b23e297b2b36a
Richard Sahlin <richard_sahlin@yahoo.com>
2015-04-09 12:36:32 +0200

    Add index constant for attribute 1 and 2 offset.

--- src/com/super2k/tiledspriteengine/TiledSpriteProgram.java
+++ src/com/super2k/tiledspriteengine/TiledSpriteProgram.java
@@ -19,48 +19,48 @@ import com.nucleus.shader.ShaderVariable.VariableType;
  */
 public class TiledSpriteProgram extends ShaderProgram {
 
-	/**
-	 * Index into uniform sprite data data where the texture fraction s (width) is
-	 */
-	public final static int UNIFORM_TEX_FRACTION_S_INDEX = 0;
-	/**
-	 * Index into uniform sprite data data where the texture fraction t (height) is
-	 */
-	public final static int UNIFORM_TEX_FRACTION_T_INDEX = 1;
-	
-	/**
-	 * Index into uniform sprite data where 1 / texture fraction w - this is used to calculate y pos from frame index
-	 */
-	public final static int UNIFORM_TEX_ONEBY_S_INDEX = 2;
+    /**
+     * Index into uniform sprite data data where the texture fraction s (width) is
+     */
+    public final static int UNIFORM_TEX_FRACTION_S_INDEX = 0;
+    /**
+     * Index into uniform sprite data data where the texture fraction t (height) is
+     */
+    public final static int UNIFORM_TEX_FRACTION_T_INDEX = 1;
 
-	public final static int ATTRIBUTE_1_OFFSET = 0;
-	public final static int ATTRIBUTE_2_OFFSET = 4;
-	
-	/**
-	 * Index into aTileSprite for x position
-	 */
-	public final static int ATTRIBUTE_SPRITE_X_INDEX = 0;
-	/**
-	 * Index into aTileSprite for y position
-	 */
-	public final static int ATTRIBUTE_SPRITE_Y_INDEX = 1;
-	/**
-	 * Index into aTileSprite texture u coordinate - this is used to calculate texture coordinate with frame.
-	 */
-	public final static int ATTRIBUTE_SPRITE_U_INDEX = 2;
-	/**
-	 * Index into aTileSprite texture v coordinate - this is used to calculate texture coordinate with frame.
-	 */
-	public final static int ATTRIBUTE_SPRITE_V_INDEX = 3;
-	/**
-	 * Index into aTileSprite frame number, this is the sprite frame number to use.
-	 */
-	public final static int ATTRIBUTE_SPRITE_FRAME_INDEX = 4;
-	/**
-	 * Index into aTileSprite z axis rotation
-	 */
-	public final static int ATTRIBUTE_SPRITE_ROTATION_INDEX = 5;
-	
+    /**
+     * Index into uniform sprite data where 1 / texture fraction w - this is used to calculate y pos from frame index
+     */
+    public final static int UNIFORM_TEX_ONEBY_S_INDEX = 2;
+
+    public final static int ATTRIBUTE_1_OFFSET = 0;
+    public final static int ATTRIBUTE_2_OFFSET = 4;
+
+    /**
+     * Index into aTileSprite for x position
+     */
+    public final static int ATTRIBUTE_SPRITE_X_INDEX = 0;
+    /**
+     * Index into aTileSprite for y position
+     */
+    public final static int ATTRIBUTE_SPRITE_Y_INDEX = 1;
+    /**
+     * Index into aTileSprite texture u coordinate - this is used to calculate texture coordinate with frame.
+     */
+    public final static int ATTRIBUTE_SPRITE_U_INDEX = 2;
+    /**
+     * Index into aTileSprite texture v coordinate - this is used to calculate texture coordinate with frame.
+     */
+    public final static int ATTRIBUTE_SPRITE_V_INDEX = 3;
+    /**
+     * Index into aTileSprite frame number, this is the sprite frame number to use.
+     */
+    public final static int ATTRIBUTE_SPRITE_FRAME_INDEX = 4;
+    /**
+     * Index into aTileSprite z axis rotation
+     */
+    public final static int ATTRIBUTE_SPRITE_ROTATION_INDEX = 5;
+
     public enum VARIABLES {
         uMVPMatrix(0, ShaderVariable.VariableType.UNIFORM),
         uSpriteData(1, ShaderVariable.VariableType.UNIFORM),
@@ -91,10 +91,12 @@ public class TiledSpriteProgram extends ShaderProgram {
         return VARIABLES.valueOf(getVariableName(variable)).index;
     }
 
+    @Override
     public int getVariableCount() {
         return VARIABLES.values().length;
     }
 
+    @Override
     public void bindAttributes(GLES20Wrapper gles, BaseRenderer renderer, Mesh mesh) throws GLException {
 
         VertexBuffer buffer = mesh.getVerticeBuffer(0);
@@ -115,18 +117,21 @@ public class TiledSpriteProgram extends ShaderProgram {
             gles.glEnableVertexAttribArray(attrib3.getLocation());
             GLUtils.handleError(gles, "glEnableVertexAttribArray3 ");
             VertexBuffer buffer3 = mesh.getVerticeBuffer(1);
-            gles.glVertexAttribPointer(attrib3.getLocation(), buffer3.getComponentCount(), buffer3.getDataType(), false,
-            		buffer3.getByteStride(), buffer3.getBuffer().position(ATTRIBUTE_2_OFFSET));
+            gles.glVertexAttribPointer(attrib3.getLocation(), buffer3.getComponentCount(), buffer3.getDataType(),
+                    false,
+                    buffer3.getByteStride(), buffer3.getBuffer().position(ATTRIBUTE_2_OFFSET));
         }
         GLUtils.handleError(gles, "glVertexAttribPointer3 ");
 
     }
 
+    @Override
     public void bindUniforms(GLES20Wrapper gles, BaseRenderer renderer, Mesh mesh) throws GLException {
         ViewFrustum viewFrustum = renderer.getViewFrustum();
         System.arraycopy(viewFrustum.getProjectionMatrix(), 0, uniformMatrices, 0, 16);
         System.arraycopy(viewFrustum.getProjectionMatrix(), 0, uniformMatrices, 16, 16);
-        gles.glUniformMatrix4fv(getShaderVariable(VARIABLES.uMVPMatrix.index).getLocation(), 2, false, uniformMatrices, 0);
+        gles.glUniformMatrix4fv(getShaderVariable(VARIABLES.uMVPMatrix.index).getLocation(), 2, false, uniformMatrices,
+                0);
         GLUtils.handleError(gles, "glUniformMatrix4fv ");
         ShaderVariable v = getShaderVariable(VARIABLES.uSpriteData.index);
         if (v != null) {
