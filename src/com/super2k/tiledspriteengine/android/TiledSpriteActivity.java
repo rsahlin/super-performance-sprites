356b8a510ed8f35a2409ca08b6e0d710221b2fb5
Richard Sahlin <richard_sahlin@yahoo.com>
2015-04-16 14:09:21 +0200

    Updated to use BaseActivity

--- src/com/super2k/tiledspriteengine/android/TiledSpriteActivity.java
+++ src/com/super2k/tiledspriteengine/android/TiledSpriteActivity.java
@@ -1,27 +1,17 @@
 package com.super2k.tiledspriteengine.android;
 
-import android.app.Activity;
 import android.opengl.GLSurfaceView;
-import android.opengl.GLSurfaceView.Renderer;
 import android.os.Bundle;
 import android.view.Window;
-import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 
-import com.nucleus.android.AndroidGLES20Wrapper;
-import com.nucleus.android.AndroidRenderer;
-import com.nucleus.android.AndroidSurfaceView;
+import com.nucleus.android.BaseActivity;
 import com.nucleus.matrix.android.AndroidMatrixEngine;
-import com.nucleus.mmi.PointerInputProcessor;
-import com.nucleus.opengl.GLES20Wrapper;
 import com.nucleus.texture.android.AndroidImageFactory;
 import com.super2k.tiledspriteengine.TiledSpriteRenderer;
 
-public class TiledSpriteActivity extends Activity {
+public class TiledSpriteActivity extends BaseActivity {
 
-    private GLSurfaceView mGLView;
-    PointerInputProcessor inputProcessor = new PointerInputProcessor();
-    Renderer renderer;
-    GLES20Wrapper gles = new AndroidGLES20Wrapper();
     TiledSpriteRenderer tiledRenderer;
 
     @Override
@@ -29,11 +19,7 @@ public class TiledSpriteActivity extends Activity {
         super.onCreate(savedInstanceState);
         tiledRenderer = new TiledSpriteRenderer(gles, new AndroidImageFactory(), new AndroidMatrixEngine(),
                 inputProcessor);
-        renderer = new AndroidRenderer(tiledRenderer);
-        mGLView = new AndroidSurfaceView(getApplicationContext(), renderer, inputProcessor);
-        mGLView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(mGLView);
+        setup(tiledRenderer, GLSurfaceView.RENDERMODE_CONTINUOUSLY, LayoutParams.FLAG_FULLSCREEN,
+                Window.FEATURE_NO_TITLE);
     }
 }
