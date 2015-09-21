e468cab3ba5a0508bdca97253c6b3572d7519d97
Richard Sahlin <richard_sahlin@yahoo.com>
2015-09-21 11:25:29 +0200

    JOGL app implementation and use JOGL base app to start

--- super-performance-sprites-jogl/src/main/java/com/super2k/tiledspriteengine/jogl/SuperSpritesJOGL.java
+++ super-performance-sprites-jogl/src/main/java/com/super2k/tiledspriteengine/jogl/SuperSpritesJOGL.java
@@ -1,7 +1,25 @@
 package com.super2k.tiledspriteengine.jogl;
 
-import com.nucleus.jogl.J2SEBaseApplication;
+import com.nucleus.jogl.NucleusApplication;
+import com.nucleus.opengl.GLESWrapper.Renderers;
+import com.super2k.tiledspriteengine.SuperSprites;
 
-public class SuperSpritesJOGL extends J2SEBaseApplication {
+public class SuperSpritesJOGL extends NucleusApplication {
 
+    public static void main(String[] args) {
+        SuperSpritesJOGL spritesApp = new SuperSpritesJOGL();
+        spritesApp.create(Renderers.GLES20);
+    }
+
+    public void create(Renderers version) {
+        super.createCore(version);
+    }
+
+    @Override
+    public void contextCreated(int width, int height) {
+        super.contextCreated(width, height);
+        SuperSprites sprites = new SuperSprites();
+        sprites.init(coreApp);
+
+    }
 }
