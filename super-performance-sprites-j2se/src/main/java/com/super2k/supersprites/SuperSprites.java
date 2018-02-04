d2fead0db10630e572bdd3b73bab51da618a83f5
Richard Sahlin <richard_sahlin@yahoo.com>
2018-02-04 15:49:08 +0100

    Bugfixes after changes to graphics-by-opengl

--- super-performance-sprites-j2se/src/main/java/com/super2k/supersprites/SuperSprites.java
+++ super-performance-sprites-j2se/src/main/java/com/super2k/supersprites/SuperSprites.java
@@ -2,7 +2,6 @@ package com.super2k.supersprites;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Random;
 
 import com.graphicsengine.component.SpriteComponent;
 import com.graphicsengine.io.GSONGraphicsEngineFactory;
@@ -13,7 +12,6 @@ import com.nucleus.SimpleLogger;
 import com.nucleus.camera.ViewFrustum;
 import com.nucleus.common.Type;
 import com.nucleus.component.ComponentNode;
-import com.nucleus.geometry.Mesh;
 import com.nucleus.io.SceneSerializer;
 import com.nucleus.mmi.MMIEventListener;
 import com.nucleus.mmi.MMIPointerEvent;
@@ -23,16 +21,12 @@ import com.nucleus.renderer.NucleusRenderer.RenderContextListener;
 import com.nucleus.renderer.Window;
 import com.nucleus.scene.LayerNode;
 import com.nucleus.scene.Node;
-import com.nucleus.scene.Node.MeshType;
 import com.nucleus.scene.NodeException;
 import com.nucleus.scene.RootNode;
 import com.nucleus.system.ComponentHandler;
-import com.nucleus.texturing.Texture2D;
-import com.nucleus.texturing.TiledTexture2D;
 import com.nucleus.vecmath.VecMath;
 import com.nucleus.vecmath.Vector2D;
 import com.super2k.supersprites.system.SuperSpriteSystem;
-import com.super2k.supersprites.system.SuperSpriteSystem.EntityData;
 
 public class SuperSprites implements MMIEventListener, RenderContextListener, ClientApplication {
 
@@ -62,28 +56,21 @@ public class SuperSprites implements MMIEventListener, RenderContextListener, Cl
     }
 
     protected final static String TILED_SPRITE_RENDERER_TAG = "TiledSpiteRenderer";
-    /**
-     * Must be set from scenetree
-     */
-    private int spritecount;
-    private int currentSprite = 0;
-    private Random random = new Random(System.currentTimeMillis());
 
     Window window;
     CoreApp coreApp;
     RootNode root;
     NucleusRenderer renderer;
     private int spriteFrames;
-    private ComponentNode component;
+    private ComponentNode componentNode;
     private SpriteComponent spriteComponent;
+    private SuperSpriteSystem system;
     private float[] pointerScale = new float[2];
     private LayerNode viewNode;
 
     public static float[] worldLimit;
     private float orthoLeft;
     private float orthoTop;
-    // Ugly fix just to enable stopping sprites
-    public static boolean process = false;
 
     public SuperSprites() {
         super();
@@ -105,12 +92,9 @@ public class SuperSprites implements MMIEventListener, RenderContextListener, Cl
     public void onInputEvent(MMIPointerEvent event) {
 
         switch (event.getAction()) {
-            case INACTIVE:
-                process = false;
-                break;
             case MOVE:
                 float[] move = event.getPointerData().getDelta(1);
-                if (move != null && component != null) {
+                if (move != null && componentNode != null) {
                     // component.getTransform().translate(move[0], move[1]);
                 }
                 releaseSprite(event.getPointerData().getCurrentPosition(), move);
@@ -118,7 +102,6 @@ public class SuperSprites implements MMIEventListener, RenderContextListener, Cl
             case ACTIVE:
                 fetchSprites();
                 releaseSprite(event.getPointerData().getCurrentPosition(), null);
-                process = true;
                 break;
             case ZOOM:
                 Vector2D zoom = event.getZoom();
@@ -143,59 +126,33 @@ public class SuperSprites implements MMIEventListener, RenderContextListener, Cl
         }
     }
 
+    /**
+     * 
+     * @param pos
+     */
     private void releaseSprite(float[] pos, float[] delta) {
-        float[] scale = root.getViewNode(Layer.SCENE).getTransform().getScale();
-        float x = (pos[0] / scale[VecMath.X]);
-        float y = (pos[1] / scale[VecMath.Y]);
-        spriteComponent.setSprite(currentSprite, x, y, 1, 1, 1, 1, 0, random.nextInt(spriteFrames));
-        int index = currentSprite * spriteComponent.getSpritedataSize()
-                + spriteComponent.getMapper().attributesPerVertex;
-
-        spriteData[index + EntityData.MOVE_VECTOR_X.index] = 0;
-        spriteData[index + EntityData.MOVE_VECTOR_Y.index] = 0;
-        spriteData[index + EntityData.ELASTICITY.index] = 0.5f + random.nextFloat() * 0.5f;
-        if (delta != null) {
-            spriteData[index + EntityData.ROTATE_SPEED.index] = delta[0];
-        }
-        // s.setScale(0.8f + random.nextFloat() * 0.5f, 0.8f + random.nextFloat() * 0.5f);
-        currentSprite++;
-        if (currentSprite > spritecount - 1) {
-            currentSprite = 0;
-        }
+        system.releaseSprite(pos);
     }
 
     /**
      * Fetches the framecount and number of sprites
      */
     private void fetchSprites() {
-        if (component != null) {
+        if (componentNode != null) {
             return;
         }
         if (viewNode == null) {
             viewNode = root.getViewNode(Layer.SCENE);
             updateNodeScale(0);
         }
-        component = (ComponentNode) root.getNodeById("root")
+        componentNode = (ComponentNode) root.getNodeById("root")
                 .getNodeByType(GraphicsEngineNodeType.spriteComponentNode.name());
-        float xpos = worldLimit[0];
-        float ypos = worldLimit[1];
-        if (component != null) {
-            spriteComponent = (SpriteComponent) component.getComponentById("spritecomponent");
+        if (componentNode != null) {
+            spriteComponent = (SpriteComponent) componentNode.getComponentById("spritecomponent");
             if (spriteComponent == null) {
                 throw new IllegalArgumentException("Could not find component");
             }
-            Mesh mesh = component.getMesh(MeshType.MAIN);
-            // TODO A method to query the mesh how many frames it supports?
-            // Maybe a way to fetch the texture from the resources?
-            TiledTexture2D tiledTexture = (TiledTexture2D) mesh.getTexture(Texture2D.TEXTURE_0);
-            spriteFrames = tiledTexture.getTileWidth() * tiledTexture.getTileHeight();
-            spritecount = spriteComponent.getCount();
-            System.out.println("Spritecount: " + spritecount + ", Spriteframes: " + spriteFrames);
-            int spriteCount = spriteComponent.getCount();
-            int height = (int) Math.sqrt(spriteCount);
-            int width = (height);
-            float deltay = ((worldLimit[1] - worldLimit[3]) / height) * 1f;
-            float deltax = ((worldLimit[2] - worldLimit[0]) / width) * 1f;
+            system = (SuperSpriteSystem) ComponentHandler.getInstance().getSystem(spriteComponent);
         }
     }
 
--- super-performance-sprites-j2se/src/main/java/com/super2k/supersprites/system/SuperSpriteSystem.java
+++ super-performance-sprites-j2se/src/main/java/com/super2k/supersprites/system/SuperSpriteSystem.java
@@ -13,7 +13,6 @@ import com.nucleus.scene.Node;
 import com.nucleus.scene.RootNode;
 import com.nucleus.system.System;
 import com.nucleus.vecmath.VecMath;
-import com.nucleus.vecmath.Vector2D;
 
 /**
  * The system for controlling the sprites defined by {@linkplain SpriteComponent}
@@ -58,13 +57,16 @@ public class SuperSpriteSystem extends System {
     private final static float TWOPI = 3.1415926f * 2;
     public final static float GRAVITY = -5;
 
+    private int spritecount;
     RootNode root;
     private LayerNode viewNode;
     public static float[] worldLimit = new float[4];
     private float orthoLeft;
     private float orthoTop;
     private boolean initialized = false;
-    private Vector2D moveVector = new Vector2D();
+    private SpriteComponent sprites;
+    private int currentSprite = 0;
+    private Random random = new Random(java.lang.System.currentTimeMillis());
 
     public SuperSpriteSystem() {
     }
@@ -77,15 +79,16 @@ public class SuperSpriteSystem extends System {
         updateNodeScale();
         SpriteComponent spriteComponent = (SpriteComponent) component;
         int spriteCount = spriteComponent.getCount();
-        float[] spriteData = spriteComponent.getSpriteData();
         PropertyMapper mapper = spriteComponent.getMapper();
         int spriteIndex = 0;
-        int readLength = spriteComponent.getSpritedataSize();
         float[] entityData = new float[EntityData.getSize()];
-        float[] v = moveVector.vector;
+        float[] spriteData = new float[mapper.attributesPerVertex];
         for (int sprite = 0; sprite < spriteCount; sprite++) {
-            spriteIndex = sprite * readLength;
+            // Fetch data from componentbuffer
+            spriteComponent.get(sprite, 0, spriteData);
             spriteComponent.get(sprite, 1, entityData);
+
+            // Do processing
             spriteData[mapper.rotateOffset + spriteIndex] += deltaTime
                     * entityData[EntityData.ROTATE_SPEED.index];
             if (spriteData[mapper.rotateOffset + spriteIndex] > TWOPI) {
@@ -100,8 +103,8 @@ public class SuperSpriteSystem extends System {
             xpos += deltaTime * entityData[EntityData.MOVE_VECTOR_X.index];
             ypos += deltaTime * entityData[EntityData.MOVE_VECTOR_Y.index];
             if (ypos < worldLimit[3]) {
-                spriteData[EntityData.MOVE_VECTOR_Y.index] = -spriteData[EntityData.MOVE_VECTOR_Y.index]
-                        * spriteData[EntityData.ELASTICITY.index];
+                entityData[EntityData.MOVE_VECTOR_Y.index] = -entityData[EntityData.MOVE_VECTOR_Y.index]
+                        * entityData[EntityData.ELASTICITY.index];
                 ypos = worldLimit[3] - (ypos - worldLimit[3]);
             }
             if (xpos > worldLimit[2]) {
@@ -117,6 +120,11 @@ public class SuperSpriteSystem extends System {
             }
             spriteData[mapper.translateOffset + spriteIndex] = xpos;
             spriteData[mapper.translateOffset + 1 + spriteIndex] = ypos;
+
+            // Put data back in componentbuffer
+            spriteComponent.put(sprite, 0, spriteData);
+            spriteComponent.put(sprite, 1, entityData);
+
         }
     }
 
@@ -135,31 +143,42 @@ public class SuperSpriteSystem extends System {
     }
 
     private void initSprites(SpriteComponent sprites) {
+        this.sprites = sprites;
         int spriteFrames = sprites.getFrameCount();
+        spritecount = sprites.getCount();
         int frame = 0;
         Random random = new Random();
-        float[] spriteData = sprites.getSpriteData();
         float rotation = 0;
+        PropertyMapper mapper = sprites.getMapper();
+        float[] spriteData = new float[mapper.attributesPerVertex];
+        float[] entityData = new float[EntityData.getSize()];
         for (int currentSprite = 0; currentSprite < sprites.getCount(); currentSprite++) {
-            int index = currentSprite * sprites.getSpritedataSize() + sprites.getMapper().attributesPerVertex;
             float[] scale = root.getViewNode(Layer.SCENE).getTransform().getScale();
-            float x = (((random.nextFloat() * 1.67f) - 0.8889f) / scale[VecMath.X]);
-            float y = ((random.nextFloat() - 0.5f) / scale[VecMath.Y]);
-            sprites.setSprite(currentSprite, x, y, 1f, 1 + random.nextFloat(), 1 + random.nextFloat(),
-                    1f, rotation, frame++);
+            spriteData[mapper.translateOffset] = (((random.nextFloat() * 1.67f) - 0.8889f) / scale[VecMath.X]);
+            spriteData[mapper.translateOffset + 1] = ((random.nextFloat() - 0.5f) / scale[VecMath.Y]);
+            spriteData[mapper.translateOffset + 2] = 1;
+
+            spriteData[mapper.scaleOffset] = random.nextFloat() + 1;
+            spriteData[mapper.scaleOffset + 1] = random.nextFloat() + 1;
+            spriteData[mapper.scaleOffset + 2] = 1;
+
+            spriteData[mapper.rotateOffset] = 0;
+            spriteData[mapper.rotateOffset + 1] = 0;
+            spriteData[mapper.rotateOffset + 2] = rotation;
+
+            spriteData[mapper.frameOffset] = frame++;
+
             rotation += 0.01f;
             if (frame >= spriteFrames) {
                 frame = 0;
             }
-            spriteData[index + EntityData.MOVE_VECTOR_X.index] = 0;
-            spriteData[index + EntityData.MOVE_VECTOR_Y.index] = 0;
-            spriteData[index + EntityData.ELASTICITY.index] = 0.5f + random.nextFloat() * 0.5f;
-            spriteData[index + EntityData.RESISTANCE.index] = random.nextFloat() * 0.03f;
+            entityData[EntityData.MOVE_VECTOR_X.index] = 0;
+            entityData[EntityData.MOVE_VECTOR_Y.index] = 0;
+            entityData[EntityData.ELASTICITY.index] = 0.5f + random.nextFloat() * 0.5f;
+            entityData[EntityData.RESISTANCE.index] = random.nextFloat() * 0.03f;
+            sprites.put(currentSprite, 0, spriteData);
+            sprites.put(currentSprite, 1, entityData);
             currentSprite++;
-            // if (currentSprite > spritecount - 1) {
-            // currentSprite = 0;
-            // }
-
         }
     }
 
@@ -176,6 +195,28 @@ public class SuperSpriteSystem extends System {
     @Override
     public int getEntityDataSize() {
         return EntityData.getSize();
+    }
+
+    /**
+     * 
+     * @param pos
+     */
+    public void releaseSprite(float[] pos) {
+        float[] scale = root.getViewNode(Layer.SCENE).getTransform().getScale();
+        float x = (pos[0] / scale[VecMath.X]);
+        float y = (pos[1] / scale[VecMath.Y]);
+        sprites.setTranslate(currentSprite, new float[] { x, y, 1 });
+        sprites.setScale(currentSprite, new float[] { 1, 1, 1 });
+        sprites.setRotate(currentSprite, new float[] { 0, 0, 1 });
+        float[] entityData = new float[EntityData.getSize()];
+        entityData[EntityData.MOVE_VECTOR_X.index] = 0;
+        entityData[EntityData.MOVE_VECTOR_Y.index] = 0;
+        entityData[EntityData.ELASTICITY.index] = 0.5f + random.nextFloat() * 0.5f;
+        sprites.put(currentSprite, 1, entityData);
+        currentSprite++;
+        if (currentSprite > spritecount - 1) {
+            currentSprite = 0;
+        }
     }
 
 }
